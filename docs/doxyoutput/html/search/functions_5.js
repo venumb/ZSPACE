689cc2de929c9834680efa9a9850dd8f6db958ab
Vishu Bhooshan <vishu.bhooshan@outlook.com>
2018-11-23 18:40:43 +0000

    updated documentation. B

--- docs/doxyoutput/html/search/functions_5.js
+++ docs/doxyoutput/html/search/functions_5.js
@@ -5,6 +5,7 @@ var searchData=
   ['fromcolumnmatrix',['fromColumnMatrix',['../classz_space_1_1z_vector.html#a12b8d25bfa4d24a70e24faeb5114931e',1,'zSpace::zVector']]],
   ['fromjson',['fromJSON',['../group__z_i_o___mesh.html#gad2f07c6da97272b61502c6fab57d83b0',1,'zSpace::fromJSON(zMesh &amp;inMesh, string infilename)'],['../group__z_i_o___graph.html#ga72f18c5d3de0d92bea72ed214d7ba159',1,'zSpace::fromJSON(zGraph &amp;inGraph, string infilename)']]],
   ['frommatrixcolumn',['fromMatrixColumn',['../group__z_vector_matrix_utilities.html#ga57229ba8064031163267f155e9236d7e',1,'zSpace']]],
-  ['frommatrixrow',['fromMatrixRow',['../classz_space_1_1z_vector.html#abeb2dd03c9b6ba91854bfb342528a534',1,'zSpace::zVector::fromMatrixRow()'],['../group__z_vector_matrix_utilities.html#ga8a6437d21ef1d59766c0660d2837b84f',1,'zSpace::fromMatrixRow()']]],
-  ['fromobj',['fromOBJ',['../group__z_i_o___mesh.html#gad60778d5e70ebdcc0bd24babf3afa810',1,'zSpace']]]
+  ['frommatrixrow',['fromMatrixRow',['../group__z_vector_matrix_utilities.html#ga8a6437d21ef1d59766c0660d2837b84f',1,'zSpace']]],
+  ['fromobj',['fromOBJ',['../group__z_i_o___mesh.html#gad60778d5e70ebdcc0bd24babf3afa810',1,'zSpace']]],
+  ['fromrowmatrix',['fromRowMatrix',['../classz_space_1_1z_vector.html#a10c955506bd52a5a845e58f680632b50',1,'zSpace::zVector']]]
 ];
